e0f24070c321f12b25c8516b602bdfdf638615b6
Alan Tseng <atseng202@gmail.com>
2019-08-22 01:23:42 -0700

    Update redux cycle for newly received messages

---
+++ frontend/actions/message_actions.js
@@ -0,0 +1,6 @@
+export const RECEIVE_MESSAGE = 'RECEIVE_MESSAGE';
+
+export const receive_message = message => ({
+  type: RECEIVE_MESSAGE,
+  message
+});
--- frontend/reducers/messages_reducer.js
+++ frontend/reducers/messages_reducer.js
@@ -1,12 +1,20 @@
 import { RECEIVE_CHANNEL } from '../actions/channel_actions';
+import { RECEIVE_MESSAGE } from '../actions/message_actions';
 
-const messagesReducer = (state = {}, action) => {
+const _initialState = {};
+
+const messagesReducer = (state = _initialState, action) => {
   Object.freeze(state);
   switch (action.type) {
     // all initial messages data for the channel are normalized 
     // during channel fetch 
     case RECEIVE_CHANNEL:
-      return action.channel.messages ?  action.channel.messages : state;
+      return action.channel.messages ?  action.channel.messages : _initialState;
+    case RECEIVE_MESSAGE:
+      return {
+        ...state, 
+        [action.message.id]: action.message
+      };
     default:
       return state;
   }
