27bad8dd67c09a280acd8094e24bc34509ff42ea
Alan Tseng <atseng202@gmail.com>
2019-10-04 16:30:27 -0700

    Update server form frontend for joining server

--- frontend/components/servers/server_form.jsx
+++ frontend/components/servers/server_form.jsx
@@ -9,21 +9,33 @@ class ServerForm extends React.Component {
     
     this.state = { 
       onLandingForm: true,
-      serverName: ""
-    }
+      onCreateForm: false, 
+      onJoinForm: false,
+      serverName: "",
+      inviteLink: ""
+    };
 
     this.closeServerForm = this.closeServerForm.bind(this);
     this.changeToServerForm = this.changeToServerForm.bind(this);
     this.changeToLandingForm = this.changeToLandingForm.bind(this);
+    this.changeToJoinServerForm = this.changeToJoinServerForm.bind(this);
 
     this.handleChange = this.handleChange.bind(this);
     
     this.handleSubmit = this.handleSubmit.bind(this);
+
+    this.handleJoin = this.handleJoin.bind(this);
+    this.resetForm = this.resetForm.bind(this);
+  }
+
+  componentDidMount() {
+    this.props.fetchSampleServers();
   }
 
   closeServerForm(event) {
     event.preventDefault();
     // if (event.currentTarget === event.target) {
+      this.setState({onLandingForm: true, onCreateForm: false, onJoinForm: false });
       this.props.closeServerForm();
       this.props.clearServerErrors();
     // }
@@ -32,18 +44,24 @@ class ServerForm extends React.Component {
   changeToServerForm(event) {
     // debugger;
     event.preventDefault();
-    this.setState({onLandingForm: false })
+    this.setState({onCreateForm:true, onLandingForm: false, onJoinForm: false });
     event.stopPropagation();
   }
 
   changeToLandingForm(event) {
     event.preventDefault();
-    this.setState({onLandingForm: true })
+    this.setState({onLandingForm: true, onCreateForm: false, onJoinForm: false });
     event.stopPropagation();
   }
 
-  handleChange(event) {
-    this.setState({serverName: event.target.value})
+  changeToJoinServerForm(event) {
+    event.preventDefault();
+    this.setState({onJoinForm: true, onCreateForm: false, onLandingForm: false});
+    event.stopPropagation();
+  }
+
+  handleChange(type) {
+    return event => this.setState({[type]: event.target.value});
   }
 
   handleSubmit(event) {
@@ -61,6 +79,32 @@ class ServerForm extends React.Component {
     );
   }
 
+  handleJoin(event) {
+    event.preventDefault();
+    const formData = new FormData();
+
+    formData.append('server[invite_link]', this.state.inviteLink);
+    const { joinServer } = this.props;
+    joinServer(formData).then(
+      (newServer) => {
+        this.props.history.push(`/servers/${newServer.server.id}`);
+        this.props.closeServerForm();
+        this.props.clearServerErrors();
+        this.resetForm();
+      }
+    );
+  }
+
+  resetForm() {
+    this.setState({
+      onLandingForm: true,
+      onCreateForm: false,
+      onJoinForm: false,
+      serverName: "",
+      inviteLink: ""
+    });
+  }
+
   render() { 
     const errorsList = (
       <ul className="serverForm-errors">
@@ -68,35 +112,54 @@ class ServerForm extends React.Component {
       </ul>
     );
 
-    const serverFormPage = this.state.onLandingForm ? 
-      (
-      <form className="serverForm-landing">
-        <header className="serverForm-header">Nice! Another server?</header>
-        <section className="serverForm-actions">
-          <div className="serverForm-createServer">
-            <h4 className="createServer-header">Create</h4>
-            <p className="createServer-body">Create a new server and invite your friends. It's free!</p>
-            <div className="createServer-icon"></div>
-            <div className="createServer-icon-attribute">Icons made by <a href="https://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
-            <button className="createServer-submit" onClick={this.changeToServerForm}>Create a server</button>
-          </div>
-        </section>
-      </form>
-      ) : (
+    const inviteLinks = (
+      <ul className="sampleInviteLinks">
+        {this.props.sampleInvites.map( (server, idx) => <li key={idx}>{server.invite_link}</li> ) }
+      </ul>
+    );
+
+    // debugger;
+
+    let currentFormPage; 
+    if (this.state.onLandingForm) {
+      currentFormPage = (
+        <form className="serverForm-landing">
+          <header className="serverForm-header">Nice! Another server?</header>
+          <section className="serverForm-actions">
+            <div className="serverForm-createServer">
+              <h4 className="createServer-header">Create</h4>
+              <p className="createServer-body">Create a new server and invite your friends. It's free!</p>
+              <div className="createServer-icon"></div>
+              <div className="createServer-icon-attribute">Icons made by <a href="https://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+              <button className="createServer-submit" onClick={this.changeToServerForm}>Create a server</button>
+            </div>
+
+            <div className="serverForm-joinServer">
+              <h4 className="joinServer-header">Join</h4>
+              <p className="joinServer-body">Enter an instant invite and join your friend's server!</p>
+              <div className="joinServer-icon"></div>
+              <div className="joinServer-icon-attribute">Icons made by <a href="https://www.flaticon.com/authors/eucalyp" title="Eucalyp">Eucalyp</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+              <button className="joinServer-submit" onClick={this.changeToJoinServerForm}>Join a server</button>
+            </div>
+          </section>
+        </form>
+      );
+    } else if (this.state.onCreateForm) {
+      currentFormPage = (
         <form className="serverForm-actual">
           <h5 className="serverForm-title">Create your server</h5>
 
           <p className="serverForm-caption">
             By creating a server, you will have access to free text chat to use amongst your friends.
-          </p>
+        </p>
 
           <section className="serverForm-inputs">
             <div className="inputs-outerWrapper">
               <label className="inputs-label">Server Name</label>
               {errorsList}
-              <input className="inputs-name" type="text" placeholder="Enter a server name" onChange={this.handleChange} value={this.state.serverName}/>
+              <input className="inputs-name" type="text" placeholder="Enter a server name" onChange={this.handleChange("serverName")} value={this.state.serverName} />
             </div>
-    
+
 
             <section className="serverForm-buttons">
               <button className="backButton" onClick={this.changeToLandingForm}>Back</button>
@@ -104,9 +167,33 @@ class ServerForm extends React.Component {
             </section>
           </section>
 
-      
         </form>
       );
+    } else {
+      currentFormPage = (
+        <form className="serverForm-actual">
+          <h5 className="serverForm-title joinServer-actual-title">Join a server</h5>
+          <p className="serverForm-caption">
+            Enter an invite link to join an existing server.
+            Here are some to get started!
+          </p>
+          {inviteLinks}
+
+          <section className="serverForm-inputs">
+            <div className="inputs-outerWrapper">
+              <label className="inputs-label">Invite Code</label>
+              {errorsList}
+              <input className="inputs-name" type="text" placeholder="Enter an invite" onChange={this.handleChange("inviteLink")} value={this.state.inviteLink}/>
+            </div>
+
+            <section className="serverForm-buttons">
+              <button className="backButton" onClick={this.changeToLandingForm}>Back</button>
+              <button className="joinButton" onClick={this.handleJoin}>Join</button>
+            </section>
+          </section>
+        </form>
+      );
+    }
 
     return (
       <ReactModal 
@@ -116,7 +203,7 @@ class ServerForm extends React.Component {
       onRequestClose={this.closeServerForm}
       shouldCloseOnOverlayClick={true}
       >
-        {serverFormPage}
+        {currentFormPage}
       </ReactModal>
 
     );
--- frontend/components/servers/server_form_container.js
+++ frontend/components/servers/server_form_container.js
@@ -1,16 +1,19 @@
 import { connect } from 'react-redux';
-import { closeServerForm, createSingleServer, clear_server_errors } from '../../actions/server_actions';
+import { closeServerForm, createSingleServer, clear_server_errors, joinSingleServer, fetchSampleServers } from '../../actions/server_actions';
 import ServerForm from './server_form';
 
 const mapStateToProps = state => ({
   serverFormOpen: state.ui.serversUI.serverFormOpen,
-  errors: state.errors.server
+  errors: state.errors.server,
+  sampleInvites: Object.values(state.entities.sampleInvites)
 });
 
 const mapDispatchToProps = dispatch => ({
   closeServerForm: () => dispatch(closeServerForm()),
   createServer: (formServer) => dispatch(createSingleServer(formServer)),
-  clearServerErrors: () => dispatch(clear_server_errors())
+  clearServerErrors: () => dispatch(clear_server_errors()),
+  joinServer: formServer => dispatch(joinSingleServer(formServer)),
+  fetchSampleServers: () => dispatch(fetchSampleServers())
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(ServerForm);
